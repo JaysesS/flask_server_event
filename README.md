0dd041782db404a930ac6315392d91fc16ab8a17
Jaysel33t <softelele@mail.ru>
2021-02-13 23:51:36 +0300

    Update

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 ### Run
 
     gunicorn3 -k eventlet --access-logfile - --bind 0.0.0.0:5000 -w 1 app:app
+
+    Write file name in input! 1 or 2.
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from flask import render_template, Response
-import time
+from flask import render_template, Response, request
+import time, os
 from time import sleep
 
 app = Flask(__name__)
@@ -8,6 +8,15 @@ app = Flask(__name__)
 def get_message():
     s = time.ctime(time.time())
     return s
+
+def get_file_content(filename):
+    path = os.path.join('files', filename + '.txt')
+    if os.path.exists(path):
+        f = open(path, 'r')
+        content = f.read()
+        f.close()
+        return content
+    return f'File {filename}.txt not found!'
 
 @app.route('/')
 def root():
@@ -17,6 +26,15 @@ def root():
 def other():
     x = sum([x for x in range(100000)])
     return render_template('test.html', value = x)
+
+@app.route('/stream_with_args')
+def stream_with_value():
+    file_name = request.args.get('file_name')
+    def eventStream():
+        while True:
+            sleep(0.1)
+            yield 'data: {}\n\n'.format(get_file_content(file_name))
+    return Response(eventStream(), mimetype="text/event-stream")
 
 @app.route('/stream')
 def stream():
